c445e71f0b18d0856dbafa9be2cbc4f2dedabf37
gwiedeman <gwiedeman@albany.edu>
2016-10-25 22:05:21 -0400

    non-working test scripts

--- ocr.py
+++ ocr.py
@@ -1,4 +1,26 @@
+#-*- coding: utf-8 -*-
+
 from wand.image import Image
-with Image(filename='color.jpg') as img:
-	img.type = 'grayscale';
-	img.save(filename='grayscale.jpg');
+import os
+from pytesseract import image_to_string
+from PIL import Image as pillow
+
+
+imageTest = "C:\\Users\\Greg\\Pictures\\textbookPhotos\\Geography\\1784-1822 Rev Jedidiah Morse\\1784 (1784) Geography Made Easy\\IMG_0654.jpg"
+
+images = "C:\\Users\\Greg\\Pictures\\textbookPhotos"
+
+	
+grayDir = os.path.join(images, "grayscale")
+bwDir = os.path.join(images, "bw")
+
+if not os.path.isdir(grayDir):
+	os.mkdir(grayDir)
+if not os.path.isdir(bwDir):
+	os.mkdir(bwDir)
+
+grayImg = os.path.join(grayDir, os.path.basename(imageTest))
+bwImg = os.path.join(bwDir,  os.path.basename(imageTest))
+
+
+text = image_to_string(pillow.open(grayImg))
