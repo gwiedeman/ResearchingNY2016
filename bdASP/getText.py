562965f19f0b25e6059a71f401adfff7e24f3590
Gregory Wiedeman <gwiedeman@albany.edu>
2016-11-02 14:03:47 -0400

    added text extracted from born digital student newspapers

---
+++ bdASP/getText.py
@@ -0,0 +1,16 @@
+import os
+from subprocess import Popen, PIPE
+
+__location__ = os.path.dirname(os.path.realpath(__file__))
+
+count = 0
+for pdfFile in os.listdir(os.path.join(__location__, "pdf")):
+	pdfPath = os.path.join(__location__, "pdf", pdfFile)
+	outPath = os.path.join(__location__, "ocr", pdfFile.split("-")[1].split(".")[0] + ".txt")
+	count = count + 1
+	
+	if 1 == 1:
+		process = Popen(['C:\Program Files\mupdf\mutool.exe', 'draw', '-F', 'txt', '-o', outPath, pdfPath], stdout=PIPE, stderr=PIPE)
+		stdout, stderr = process.communicate()
+		print (stdout)
+		print (stderr)
